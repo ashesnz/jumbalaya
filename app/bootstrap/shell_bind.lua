2318297eaed2b2f5e8d54dfaa92909699c0ca3c6
Ashwin Thakur <ashesnz@gmail.com>
2026-09-12 07:38:35 +1000

    Engine modification

---
+++ app/bootstrap/shell_bind.lua
@@ -0,0 +1,13 @@
+--[[ app/bootstrap/shell_bind.lua - Wire app callbacks into jumbalaya-engine.shell ]]
+
+local shell = require("jumbalaya-engine.shell")
+
+local M = {}
+
+function M.install()
+	shell.bind_app_events(require("app.bootstrap.app_events"))
+	shell.bind_funcs(require("app.callbacks.funcs"))
+	shell.bind_action_dispatch(require("app.input.action_dispatch"))
+end
+
+return M
